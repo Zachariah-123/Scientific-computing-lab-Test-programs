15bbb40afe04dd554526a635dc18660b94e7f32c
Zachariah-123 <sarosh.zachariah@gmail.com>
2024-11-11 21:49:54 +0530

    Update question 5.py

--- question 5.py
+++ question 5.py
@@ -2,9 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 def f(n):
     prod=0
+    flag = 0
     for i in range(1,n+1,2):
-        prod+=1/i
-    return prod
-
+        prod+=((-1)**flag)/i
+        flag+=1
+    return 4*prod
 print(f"Hundred terms value goes to {f(100)}")
 print(f"Ten terms value goes to {f(10)}")
+
